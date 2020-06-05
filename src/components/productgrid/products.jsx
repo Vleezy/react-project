018b24077364951f5b09b5593567760e46a5f0f2
havocwreckage <havoc.wreckage@gmail.com>
2020-06-05 11:11:39 -0400

    WIP

--- src/components/productgrid/products.jsx
+++ src/components/productgrid/products.jsx
@@ -1,27 +1,22 @@
-import React, { Component } from "react";
-import ProductData from "../../data/products.json";
+import React from "react";
 import "./products.css";
 
-class products extends Component {
-  render() {
-    return (
-      <>
-        <div class="container">
-          {ProductData.map((productDetail, index) => {
-            return (
-              <div class="grid-item ">
-                <h2>{productDetail.name}</h2>
+const Products = ({items}) => {
+  return (
+    <div class="container">
+      {items.map((productDetail, index) => {
+        return (
+          <div class="grid-item ">
+            <h2>{productDetail.name}</h2>
             <img src={productDetail.img} alt="ok"></img>
-                <div className="p">${productDetail.price}</div>
-                <span class="item_type">{productDetail.type}</span>
+            <div className="p">${productDetail.price}</div>
+            <span class="item_type">{productDetail.type}</span>
             <div>{productDetail.specs}</div>
-              </div>
-            );
-          })}
-        </div>
-      </>
-    );
-  }
-}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
-export default products;
+export default Products;
--- src/pages/product.jsx
+++ src/pages/product.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useEffect,  useState } from "react";
+
 import Products from "../components/productgrid/products";
+import ProductData from "../data/products.json";
 
 const Product = () => {
+
+  const [processor, setProcessor] = useState('default');
+  const [price, setPrice] = useState({min: 0, max: 999999999});
+  const filterProcessor = p => {
+    if (processor === "default") {
+      return true;
+    } 
+    return processor === p.type
+  }
+
+
+
+  const filterPrice = p => {
+    return p.price > price.min && p.price < price.max;
+  }
+
+  const items = ProductData.filter(filterProcessor).filter(filterPrice)
+  
+
   return (
     <>
       <main class="heroimg"></main>
@@ -9,9 +30,9 @@ const Product = () => {
         <label for="filterSelect" class="filters">
           Filter by:
         </label>
-        <select id="filterSelect" onchange="filterItems()">
+        <select id="filterSelect" onChange={({target: { value }}) => setProcessor(value)}>
           <option value="default">All</option>
-          <option value="AMD">AMD</option>
+          <option value="Amd">AMD</option>
           <option value="Intel">Intel</option>
         </select>
       </div>
@@ -19,14 +40,15 @@ const Product = () => {
         <label for="priceSelect" class="filters">
           Filter Price:
         </label>
-        <select id="priceSelect" onchange="priceItems()">
-          <option value="default">All</option>
-          <option value="0-1000">$0 - $1000</option>
-          <option value="1000-1500">$1000 - $1500</option>
-          <option value="1500-2000">$1500 - $2000</option>
-          <option value="2000-10000">$2000 - max</option>
+        <select id="priceSelect" onChange={({target: { value }}) => setPrice(value)}>
+          <option value={{min:0, max:99999999999}}>All</option>
+          <option value={{min:0, max:1000}}>$0 - $1000</option>
+          <option value={{min:1000, max:1500}}>$1000 - $1500</option>
+          <option value={{min:1500, max:2000}}>$1500 - $2000</option>
+          <option value={{min:2000, max:99999999999}}>$2000 - max</option>
         </select>
-        <Products />
+
+        <Products items={items} />
       </div>
     </>
   );
